75d55683f84e594bc6f1896152e295ee7806bdb2
Stéphane Caron <stephane.caron@normalesup.org>
2022-10-26 17:37:12 +0200

    Move validation functions to separate files

--- qpsolvers_benchmark/problem.py
+++ qpsolvers_benchmark/problem.py
@@ -15,13 +15,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Union
+
 import numpy as np
 import scipy.io as spio
 import scipy.sparse as spa
-from numpy import linalg
 
 
 class Problem:
+
+    P: Union[np.array, spa.csc_matrix]
+    q: np.array
+    G: Union[np.array, spa.csc_matrix]
+    h: np.array
+    A: Union[np.array, spa.csc_matrix]
+    b: np.array
+    lb: np.array
+    ub: np.array
+
     def __init__(self, P, q, G, h, A, b, lb, ub):
         """
         Quadratic program in qpsolvers format.
@@ -58,26 +69,26 @@ class Problem:
         assert A.shape == (m, n)
         lb = l[-n:]
         ub = u[-n:]
-        A_c = A[:-n]
+        C = A[:-n]
         l_c = l[:-n]
         u_c = u[:-n]
-        return Problem.from_double_sided_ineq(P, q, A_c, l_c, u_c, lb, ub)
+        return Problem.from_double_sided_ineq(P, q, C, l_c, u_c, lb, ub)
 
     @staticmethod
-    def from_double_sided_ineq(P, q, A, l, u, lb, ub):
+    def from_double_sided_ineq(P, q, C, l, u, lb, ub):
         """
         Load problem from double-sided inequality format:
 
         .. code::
 
             minimize        0.5 x^T P x + q^T x
-            subject to      l <= A x <= u
+            subject to      l <= C x <= u
                             lb <= x <= ub
 
         Args:
             P: Cost matrix.
             q: Cost vector.
-            A: Constraint inequality matrix.
+            C: Constraint inequality matrix.
             l: Constraint lower bound.
             u: Constraint upper bound.
             lb: Box lower bound.
@@ -85,10 +96,10 @@ class Problem:
         """
         bounds_are_equal = u - l < 1e-10
         eq_rows = np.where(bounds_are_equal)
-        eq_matrix = A[eq_rows]
+        eq_matrix = C[eq_rows]
         eq_vector = u[eq_rows]
         ineq_rows = np.where(np.logical_not(bounds_are_equal))
-        ineq_matrix = spa.vstack([A[ineq_rows], -A[ineq_rows]], format="csc")
+        ineq_matrix = spa.vstack([C[ineq_rows], -C[ineq_rows]], format="csc")
         ineq_vector = np.hstack([u[ineq_rows], -l[ineq_rows]])
         return Problem(
             P, q, ineq_matrix, ineq_vector, eq_matrix, eq_vector, lb, ub
@@ -96,62 +107,12 @@ class Problem:
 
     def constraints_as_double_sided_ineq(self):
         """
-        ...
+        Get problem constraints as double-sided inequalities.
+
+        Returns:
+            Tuple ``(C, l, u)`` corresponding to ``l <= C x <= u``.
         """
         C = spa.vstack([self.G, self.A, spa.eye(self.n)], format="csc")
         l = np.hstack([np.full(self.h.shape, -np.infty), self.b, self.lb])
         u = np.hstack([self.h, self.b, self.ub])
         return C, l, u
-
-    def is_valid_primal_solution(self, x, eps_abs: float) -> bool:
-        """
-        Validate optimality condition of a given primal solution.
-
-        Args:
-            x: Primal solution.
-            eps_abs: Absolute tolerance.
-
-        Returns:
-            True if and only if (x, y) is a valid primal-dual solution.
-
-        Note:
-            This function is adapted from `is_qp_solution_optimal` in
-            proxqp_benchmark. The original function included the relative
-            tolerance parameter specified in the OSQP paper, set to zero.
-        """
-        C, l, u = self.constraints_as_double_sided_ineq()
-        C_x = C.dot(x)
-        primal_residual = np.minimum(C_x - l, 0.0) + np.maximum(C_x - u, 0.0)
-        primal_error = linalg.norm(primal_residual, np.inf)
-        if primal_error > eps_abs:
-            print(f"Error in primal residual: {primal_error} > {eps_abs}")
-            return False
-
-        return True
-
-    def is_valid_dual_solution(self, y, x, eps_abs: float) -> bool:
-        """
-        Validate optimality condition of a given primal solution.
-
-        Args:
-            x: Primal solution.
-            y: Dual solution.
-            eps_abs: Absolute tolerance.
-
-        Returns:
-            True if and only if (x, y) is a valid primal-dual solution.
-
-        Note:
-            This function is adapted from `is_qp_solution_optimal` in
-            proxqp_benchmark. The original function included the relative
-            tolerance parameter specified in the OSQP paper, set to zero.
-        """
-        P = self.P
-        q = self.q
-        C, _, _ = self.constraints_as_double_sided_ineq()
-        dual_residual = P.dot(x) + q + C.T.dot(y)
-        dual_error = linalg.norm(dual_residual, np.inf)
-        if dual_error > eps_abs:
-            print(f"Error in dual residual: {dual_error} > {eps_abs}")
-            return False
-        return True
