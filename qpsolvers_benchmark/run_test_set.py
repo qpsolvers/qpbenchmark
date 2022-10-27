8aa01d9765613b120ea7b7eddb401f81935464a1
Stéphane Caron <stephane.caron@normalesup.org>
2022-10-27 18:42:45 +0200

    WIP: Abstract base class for test sets

---
+++ qpsolvers_benchmark/run_test_set.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Run a given test set.
+"""
+
+from typing import Any, Dict
+
+from .results import Results
+from .test_sets import TestSet
+
+
+def run_test_set(
+    test_set: TestSet,
+    solver_settings: Dict[str, Dict[str, Any]],
+    results: Results,
+) -> None:
+    problem_number = 1
+    for solver, settings in solver_settings.items():
+        for problem in test_set:
+            print(
+                f"Running problem #{problem_number} ({problem.name}) "
+                f"with {solver}..."
+            )
+            solution, duration_us = problem.solve(
+                solver=solver, **solver_settings[solver]
+            )
+            results.update(problem, solver, solution, duration_us)
+            problem_number += 1
+            if problem_number > 5:
+                break
---
+++ qpsolvers_benchmark/test_sets/test_set.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Base class for test sets.
+"""
+
+import abc
+from typing import Iterator
+
+from ..problem import Problem
+
+
+class TestSet(abc.ABC):
+    @abc.abstractproperty
+    def name(self) -> str:
+        """
+        Name of the test set.
+        """
+
+    @abc.abstractmethod
+    def __iter__(self) -> Iterator[Problem]:
+        """
+        Yield test test problems one by one.
+        """
