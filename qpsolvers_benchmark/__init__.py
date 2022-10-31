ffecdba55bda6f8df795580481231ee77a65a35e
Stéphane Caron <stephane.caron@normalesup.org>
2022-10-31 11:16:41 +0100

    Export Results from qpsolvers_benchmark

--- qpsolvers_benchmark/__init__.py
+++ qpsolvers_benchmark/__init__.py
@@ -19,14 +19,16 @@
 Benchmark for quadratic programming solvers available in Python.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.0rc0"
 
 from .problem import Problem
+from .results import Results
 from .solver_settings import SolverSettings
 from .test_set import TestSet
 
 __all__ = [
     "Problem",
+    "Results",
     "SolverSettings",
     "TestSet",
 ]
