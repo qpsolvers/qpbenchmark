fef85f0c4131ca5edbcc6e674889879626d41e35
Stéphane Caron <stephane.caron@normalesup.org>
2022-10-31 10:50:28 +0100

    Update MM test report

--- qpsolvers_benchmark/test_sets/maros_meszaros.py
+++ qpsolvers_benchmark/test_sets/maros_meszaros.py
@@ -26,8 +26,10 @@ from typing import Dict, Iterator
 import yaml
 
 from ..problem import Problem
+from ..solver_settings import SolverSettings
+from ..spdlog import logging
 from ..test_set import TestSet
-from ..utils import bool_as_emoji, get_cpu_info
+from ..utils import get_cpu_info
 
 
 class MarosMeszaros(TestSet):
@@ -69,20 +71,16 @@ class MarosMeszaros(TestSet):
         date = str(datetime.datetime.now(datetime.timezone.utc))
         cpu_info = get_cpu_info()
 
-        found_df = self.results.build_found_df()
-        found_table = found_df.to_markdown(index=True)
-        found_table = found_table.replace(
-            "False", bool_as_emoji(False) + " " * 3
-        )
-        found_table = found_table.replace(
-            "True", bool_as_emoji(True) + " " * 3
-        )
-
         success_rate_df = self.results.build_success_rate_df()
         success_rate_table = success_rate_df.to_markdown(index=True)
         success_rate_table = success_rate_table.replace(" 100    ", " **100**")
 
-        geometric_mean_df = self.results.build_geometric_mean_df()
+        geometric_mean_df = self.results.build_geometric_mean_df(
+            time_limits={
+                key: settings.time_limit
+                for key, settings in self.solver_settings.items()
+            }
+        )
         geometric_mean_table = geometric_mean_df.to_markdown(
             index=True, floatfmt=".1f"
         )
@@ -100,19 +98,40 @@ Precentage of problems each solver is able to solve:
 
 {success_rate_table}
 
-## Shifted geometric mean
+Rows are solvers and columns are solver settings.
 
-The shifted geometric mean shows the factor at which a given solver is slower
-than the fastest one over the test set. It is commonly used in [benchmarks for
+## Computation time
+
+We compare solver computation times using the shifted geometric mean.
+
+**Intuition:** a solver with a shifted geometric mean of Y is Y times slower
+than the best solver over the test set.
+
+### Details
+
+There is a different ranking of solver runtimes for each problem in the test
+set. To aggregate those rankings into a single metric over the whole test set,
+we use the shifted geometric mean, which is a standard in [benchmarks for
 optimization software](http://plato.asu.edu/bench.html).
+
+The shifted geometric mean is a slowdown factor compared to the best solver
+over the whole test set. It has the advantage of being compromised by neither
+large outliers (as opposed to the arithmetic mean) nor by small outliers (in
+contrast to the geometric geometric mean). The best solvers have a shifted
+geometric mean close to one:
+
+### Results
 
 {geometric_mean_table}
 
-## Details
+Rows are solvers and columns are solver settings.
 
-### Success
+## Precision
 
-{found_table}
+### Cost errors
+
+### Constraint errors
+
 """
             )
 
