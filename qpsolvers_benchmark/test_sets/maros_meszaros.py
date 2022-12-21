ee400f0ca4d03f105b8bdbe62481efffc095c802
Stéphane Caron <stephane.caron@normalesup.org>
2022-12-21 18:31:08 +0100

    Problem inherits from qpsolvers.Problem

--- qpsolvers_benchmark/test_sets/maros_meszaros.py
+++ qpsolvers_benchmark/test_sets/maros_meszaros.py
@@ -23,6 +23,10 @@ import json
 import os.path
 from typing import Dict, Iterator, Optional
 
+import numpy as np
+import scipy.io as spio
+import scipy.sparse as spa
+
 from ..problem import Problem
 from ..solver_settings import SolverSettings
 from ..test_set import TestSet
@@ -95,11 +99,98 @@ class MarosMeszaros(TestSet):
         self.data_dir = data_dir
         self.optimal_costs = optimal_costs
 
+    def load_problem_from_mat_file(self, path):
+        """
+        Load problem from MAT file.
+
+        Args:
+            path: Path to file.
+
+        Notes:
+            We assume that matrix files result from calling `sif2mat.m` in
+            proxqp_benchmark. In particular, ``A = [sparse(A_c); speye(n)];``
+            and the infinity constant is set to 1e20.
+        """
+        assert path.endswith(".mat")
+        name = os.path.basename(path)[:-4]
+
+        mat_dict = spio.loadmat(path)
+        P = mat_dict["P"].astype(float).tocsc()
+        q = mat_dict["q"].T.flatten().astype(float)
+        r = mat_dict["r"].T.flatten().astype(float)[0]
+        A = mat_dict["A"].astype(float).tocsc()
+        l = mat_dict["l"].T.flatten().astype(float)
+        u = mat_dict["u"].T.flatten().astype(float)
+        n = mat_dict["n"].T.flatten().astype(int)[0]
+        m = mat_dict["m"].T.flatten().astype(int)[0]
+        assert A.shape == (m, n)
+
+        # Infinity constant is 1e20
+        A[A > +9e19] = +np.inf
+        l[l > +9e19] = +np.inf
+        u[u > +9e19] = +np.inf
+        A[A < -9e19] = -np.inf
+        l[l < -9e19] = -np.inf
+        u[u < -9e19] = -np.inf
+
+        # A = vstack([C, spa.eye(n)])
+        lb = l[-n:]
+        ub = u[-n:]
+        C = A[:-n]
+        l_c = l[:-n]
+        u_c = u[:-n]
+
+        return self.convert_problem_from_double_sided(
+            P, q, C, l_c, u_c, lb, ub, name=name, cost_offset=r
+        )
+
+    @staticmethod
+    def convert_problem_from_double_sided(
+        P, q, C, l, u, lb, ub, name: str, cost_offset: float = 0.0
+    ):
+        """
+        Load problem from double-sided inequality format:
+
+        .. code::
+
+            minimize        0.5 x^T P x + q^T x + cost_offset
+            subject to      l <= C x <= u
+                            lb <= x <= ub
+
+        Args:
+            P: Cost matrix.
+            q: Cost vector.
+            C: Constraint inequality matrix.
+            l: Constraint lower bound.
+            u: Constraint upper bound.
+            lb: Box lower bound.
+            ub: Box upper bound.
+            name: Problem name.
+            cost_offset: Cost offset.
+        """
+        bounds_are_equal = u - l < 1e-10
+
+        eq_rows = np.where(bounds_are_equal)
+        A = C[eq_rows]
+        b = u[eq_rows]
+
+        ineq_rows = np.where(np.logical_not(bounds_are_equal))
+        G = spa.vstack([C[ineq_rows], -C[ineq_rows]], format="csc")
+        h = np.hstack([u[ineq_rows], -l[ineq_rows]])
+        h_finite = h < np.inf
+        if not h_finite.all():
+            G = G[h_finite]
+            h = h[h_finite]
+
+        return Problem(
+            P, q, G, h, A, b, lb, ub, name=name, cost_offset=cost_offset
+        )
+
     def __iter__(self) -> Iterator[Problem]:
         for fname in os.listdir(self.data_dir):
             if fname.endswith(".mat"):
                 mat_path = os.path.join(self.data_dir, fname)
-                problem = Problem.from_mat_file(mat_path)
+                problem = self.get_problem_from_mat_file(mat_path)
                 if problem.name in self.optimal_costs:
                     problem.optimal_cost = self.optimal_costs[problem.name]
                 yield problem
