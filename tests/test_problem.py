9a51b0a2636a9f7f1f3c044a6817d0c891fbfea2
Stéphane Caron <stephane.caron@normalesup.org>
2023-12-21 14:08:53 +0100

    Lint unit test

--- tests/test_problem.py
+++ tests/test_problem.py
@@ -17,7 +17,17 @@ from qpbenchmark.problem import Problem
 
 class TestUtils(unittest.TestCase):
     def test_load(self):
-        problem = Problem(P=np.eye(3), q=np.zeros(3), G=None, h=None, A=None, b=None, lb=None, ub=None, name="TEST")
+        problem = Problem(
+            P=np.eye(3),
+            q=np.zeros(3),
+            G=None,
+            h=None,
+            A=None,
+            b=None,
+            lb=None,
+            ub=None,
+            name="TEST",
+        )
         fpath = os.path.join(tempfile.gettempdir(), "FOOBAR.npz")
         problem.save(fpath)
         loaded = Problem.load(fpath)
