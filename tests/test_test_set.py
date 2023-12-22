115cb6b99ccf8074332b4bd8abeac5ca060bbf12
Stéphane Caron <stephane.caron@normalesup.org>
2023-12-22 10:53:52 +0100

    Add unit test for solver timeouts

--- tests/test_test_set.py
+++ tests/test_test_set.py
@@ -53,3 +53,48 @@ class TestTestSet(unittest.TestCase):
         self.test_set.known_solver_issues.add(("foo", "bar"))
         self.assertTrue(self.test_set.skip_solver_issue(foo, "bar"))
         self.assertFalse(self.test_set.skip_solver_issue(foo, "baz"))
+
+    def test_skip_solver_timeout_specific(self):
+        foo = custom_problem(name="foo")
+        self.test_set.known_solver_timeouts[("foo", "bar", "default")] = 1.0
+        self.assertFalse(
+            self.test_set.skip_solver_timeout(
+                1.0, foo, "unknown_solver", "settings"
+            )
+        )
+        self.assertFalse(
+            self.test_set.skip_solver_timeout(
+                2.0,  # greater than 1.0
+                foo,
+                "bar",
+                "default",
+            )
+        )
+        self.assertTrue(
+            self.test_set.skip_solver_timeout(
+                0.5,  # lower than 1.0
+                foo,
+                "bar",
+                "default",
+            )
+        )
+
+    def test_skip_solver_timeout_pattern(self):
+        foo = custom_problem(name="foo")
+        self.test_set.known_solver_timeouts[("foo", "bar", "*")] = 1.0
+        self.assertFalse(
+            self.test_set.skip_solver_timeout(
+                2.0,  # greater than 1.0
+                foo,
+                "bar",
+                "flip",
+            )
+        )
+        self.assertTrue(
+            self.test_set.skip_solver_timeout(
+                0.5,  # lower than 1.0
+                foo,
+                "bar",
+                "flop",
+            )
+        )
